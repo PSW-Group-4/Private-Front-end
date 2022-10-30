ad66e1872a3a08bf3e393b60bc89317da36f54e7
MiskLG <misklg283@gmail.com>
2022-10-31 00:05:41 +0100

    Created base module setup for maps

---
+++ hospital-fe-main/src/app/modules/maps/maps.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditItemComponent } from './containers/edit-item/edit-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    EditItemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MapsModule { }
