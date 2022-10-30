b8b64cee83a999b9b0db878dc164bb67b4374e54
isidorapoznanovic <isidorapoznanovic1@gmail.com>
2022-10-30 15:43:07 +0100

    Added check blood supplies dialog

--- hospital-fe-main/src/app/app.module.ts
+++ hospital-fe-main/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule,routingComponents } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MaterialModule } from "./material/material.module";
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
 
 import { HospitalModule } from "./modules/hospital/hospital.module";
 import { PagesModule } from "./modules/pages/pages.module";
 import { ManagerFeedbackComponent } from './manager-feedback/manager-feedback.component';
 import { DoctorAppointmentsModule } from "./modules/doctor-appointments/doctor-appointments.module";
+import { BloodBanksComponent, DialogOverviewExampleDialog } from './modules/hospital/blood-banks/blood-banks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ManagerFeedbackComponent
+    ManagerFeedbackComponent,
+    DialogOverviewExampleDialog
   ],
   imports: [
     BrowserModule,
@@ -22,13 +29,18 @@ import { DoctorAppointmentsModule } from "./modules/doctor-appointments/doctor-a
     BrowserAnimationsModule,
     HttpClientModule,
     MaterialModule,
-
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    NgbModule,
+    FormsModule,
     PagesModule,
     HospitalModule,
+    MatNativeDateModule,
     
     DoctorAppointmentsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, BloodBanksComponent]
 })
 export class AppModule { }
--- hospital-fe-main/src/app/modules/hospital/blood-banks/blood-banks.component.ts
+++ hospital-fe-main/src/app/modules/hospital/blood-banks/blood-banks.component.ts
@@ -1,8 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { Router } from '@angular/router';
 import { BloodBank } from 'src/app/modules/hospital/model/blood-bank.model';
 import { BloodBankService } from 'src/app/modules/hospital/services/blood-bank.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EMPTY, Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  quantity: number;
+  bloodType: string;
+}
 
 @Component({
   selector: 'app-blood-banks',
@@ -11,10 +22,12 @@ import { BloodBankService } from 'src/app/modules/hospital/services/blood-bank.s
 })
 export class BloodBanksComponent implements OnInit {
   public dataSource = new MatTableDataSource<BloodBank>();
-  public displayedColumns = ['Name', 'Server Address', 'Email Address'];
+  public displayedColumns = ['Name', 'Server Address', 'Email Address', 'Blood supplies'];
   public bloodBanks: BloodBank[] = [];
-  
-  constructor(private bloodBankService: BloodBankService, private router: Router) { }
+  quantity: number = 0;
+  bloodType: string = "";
+
+  constructor(private bloodBankService: BloodBankService, private router: Router, private modalService: NgbModal, public dialog: MatDialog) { }
   
   ngOnInit(): void {
     this.bloodBankService.getBloodBanks().subscribe(res => {
@@ -24,5 +37,60 @@ export class BloodBanksComponent implements OnInit {
   }
   public addBloodBank() {
     this.router.navigate(['/bloodBanks/add']);
+  }
+  
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {bloodType: this.bloodType, quantity: this.quantity},
+    });
+
+    dialogRef.componentInstance.location = 'http://localhost:4200';
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if (result){
+        [this.quantity, this.bloodType] = result;
+        console.log(this.quantity, this.bloodType);
+      }
+      [this.quantity, this.bloodType] = [0, ''];
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+  myControl = new FormControl('');
+  public location: string = '';
+  filteredOptions: Observable<string[]> = EMPTY;
+  options: string[] = ['0-', '0+', 'A-', 'A+', 'B-', 'B+', 'AB-', 'AB+'];
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {}
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+      console.log(this.location);
+  }
+  
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  public form: FormGroup = new FormGroup({
+    bloodType: new FormControl(''),
+    quantity: new FormControl(''),
+  });
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
