ecf0a234ffefa2fa1bb940f427acbbc164beaab7
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-10-31 23:14:57 +0100

    Added model

--- hospital-fe-main/src/app/manager-feedback/Service/Feedback/feedback.service.ts
+++ hospital-fe-main/src/app/manager-feedback/Service/Feedback/feedback.service.ts
@@ -2,27 +2,27 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ConstSettings } from '../../constSettings';
+import { FeedbackManagerResponseDto } from '../../Model/feedbackManagerResponseDto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
-  apiHost: string = 'http://localhost:16177/';
-  headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
 
   constructor(private http: HttpClient) { }
 
-    getAllFeedbacks(): Observable<Object> {
-    return this.http.get<Object[]>(ConstSettings.apiHost + 'api/Feedback', { headers: ConstSettings.standardHeader });
+    getAllFeedbacks(): Observable<FeedbackManagerResponseDto[]> {
+    return this.http.get<FeedbackManagerResponseDto[]>(ConstSettings.apiHost  + 'api/Feedback', { headers:  ConstSettings.standardHeader });
   }
 
 
    publishFeedback(id: any): Observable<any> {
-    return this.http.get<any>(ConstSettings.apiHost  + 'api/feedback/publish/' + id, {headers:  ConstSettings.standardHeader});
+    return this.http.get<any>(ConstSettings.apiHost + 'api/feedback/publish/' + id, {headers: ConstSettings.standardHeader});
    }
 
    hideFeedback(id: any): Observable<any> {
-    return this.http.get<any>(ConstSettings.apiHost + 'api/feedback/hide/' + id, {headers:  ConstSettings.standardHeader});
+    return this.http.get<any>(ConstSettings.apiHost  + 'api/feedback/hide/' + id, {headers: ConstSettings.standardHeader});
   }
 
 
