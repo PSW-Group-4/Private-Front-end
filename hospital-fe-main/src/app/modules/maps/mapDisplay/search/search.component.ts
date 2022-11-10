e4f6aa0f429b7d4fdfad83953cd8409c86686f97
MiskLG <misklg283@gmail.com>
2022-11-10 23:25:17 +0100

    Added onclick events for methods and their body
    This part needs a bit of work still, this code smells

--- hospital-fe-main/src/app/modules/maps/mapDisplay/search/search.component.ts
+++ hospital-fe-main/src/app/modules/maps/mapDisplay/search/search.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RoomMap } from '../../models/room-map.model';
 import { MapsFacade } from '../../maps.facade';
+import { Router } from '@angular/router';
+import { Floor } from '../../models/floor.model';
+import { FloorMap } from '../../models/floor-map.model';
+import { BuildingMap } from '../../models/building-map.model';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -11,13 +16,52 @@ export class SearchComponent implements OnInit {
 
   maps$:RoomMap[]=[];
   searchText = '';
-  constructor(private mapsFacade:MapsFacade) {
+  floors : FloorMap[] = [];
+  buildings : BuildingMap[] = [];
+  constructor(private router: Router, private mapsFacade:MapsFacade) {
+      this.mapsFacade.getFloorMaps$().subscribe({
+        next : (v) => this.floors = v
+      })
+      this.mapsFacade.getBuildingMaps$().subscribe({
+        next : (v) => this.buildings = v
+      })
    }
 
   ngOnInit(): void {
     this.mapsFacade.getRoomMaps$().subscribe(res=>{
       this.maps$ = res;
     })
+  }
+
+  async highlight(room: RoomMap): Promise<void> {
+    this.router.navigate([this.getUrl(room)]);
+    await delay(100)
+    this.mapsFacade.setSelectedRoomMap(room);
+    
+  }
+
+  getUrl(roomMap: RoomMap): string {
+    var buildingId = '';
+    this.buildings.forEach(building => {
+      building.building.floorList.forEach(floor => {
+        floor.roomList.forEach(room => {
+          if(roomMap.room.id == room.id) {
+            buildingId = building.id;
+          }
+        });
+      });
+    });
+
+    var floorId = '';
+    this.floors.forEach(floor => {
+      floor.floor.roomList.forEach(room => {
+        if(roomMap.room.id == room.id) {
+          floorId = floor.id;
+        }
+      });
+    });
+
+    return "room-maps/"+floorId+"/"+buildingId;
   }
 
 }
