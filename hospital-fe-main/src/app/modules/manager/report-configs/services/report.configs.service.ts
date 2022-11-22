b2ac745a244852fda3df0715a41ae0fe17992c32
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-11-22 23:44:42 +0100

    Init and edit existing reports.

---
+++ hospital-fe-main/src/app/modules/manager/report-configs/services/report.configs.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ConstSettings } from '../../manager-feedback/constSettings';
+import { ReportConfigurationDto } from '../model/reportConfigurationDto.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportConfigsService {
+
+  constructor(private http: HttpClient) { 
+
+  }
+  getAllConfigs() :Observable<ReportConfigurationDto[]>{
+    return this.http.get<ReportConfigurationDto[]>('http://localhost:5000/'  + 'api/ReportConfiguration', { headers:  ConstSettings.standardHeader });
+  }
+  updateReport(report : ReportConfigurationDto): Observable<ReportConfigurationDto> {
+    return this.http.post<any>('http://localhost:5000/'  + 'api/ReportConfiguration',report, { headers:  ConstSettings.standardHeader });
+  }
+}
