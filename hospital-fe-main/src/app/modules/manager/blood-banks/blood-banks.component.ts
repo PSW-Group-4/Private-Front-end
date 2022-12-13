aa8b38cac6ca6a3c6b2509b7c4710519f48eb466
Sanja Petrović <sanjica.petrovic7@gmail.com>
2022-12-13 20:43:18 +0100

    More routing

--- hospital-fe-main/src/app/modules/manager/blood-banks/blood-banks.component.ts
+++ hospital-fe-main/src/app/modules/manager/blood-banks/blood-banks.component.ts
@@ -35,7 +35,7 @@ export class BloodBanksComponent implements OnInit {
     })
   }
   public addBloodBank() {
-    this.router.navigate(['/bloodBanks/add']);
+    this.router.navigate(['bloodBanks/add']);
   }
 
   openDialog(): void {
