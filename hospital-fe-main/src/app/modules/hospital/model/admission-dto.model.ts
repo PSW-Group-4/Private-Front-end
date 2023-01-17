9cd324263d0b768d5b2e37940ee4f8d92c5b08d0
stefaN-N-N <stefanlepsanovic@gmail.com>
2023-01-17 19:32:07 +0100

    model reason added

--- hospital-fe-main/src/app/modules/hospital/model/admission-dto.model.ts
+++ hospital-fe-main/src/app/modules/hospital/model/admission-dto.model.ts
@@ -1,18 +1,19 @@
 import { PatientRoom } from "./patient-room.model";
 import { Patient } from "./patient.model";
+import { Reason } from "./reason.model";
 import { Room } from "./room.model";
 
 export class AdmissionDto {    
     roomId: string = '';
     patientId: string = '';
-    reason: string = '';
+    reasonText: Reason = new Reason();
     arrivalDate: string = '';
     
 
     public constructor(obj?: any) {
         if (obj) {            
             this.patientId = obj.patient;
-            this.reason = obj.reason;
+            this.reasonText = obj.reasonText;
             this.roomId = obj.room;
             this.arrivalDate = obj.arrivalDate;     
             
